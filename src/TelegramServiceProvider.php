612b4874c9b97b4b19dd1cfc3f6caef4f6327eed
Mahmoud Hosseini <hosseinimh@gmail.com>
2023-01-17 06:47:07 +0330

    Initial commit

---
+++ src/TelegramServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Hosseinimh\Telegram;
+
+use Illuminate\Support\ServiceProvider;
+
+class TelegramServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+    }
+}
